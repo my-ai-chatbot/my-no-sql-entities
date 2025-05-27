cfd633ac8330a1e4d201a2f8ef42f11a4c54c80a
amigin <amigin@gmail.com>
2025-05-27 14:10:31 +0300

    Added last edited and who

--- src/system_prompts.rs
+++ src/system_prompts.rs
@@ -6,6 +6,8 @@ service_sdk::macros::use_my_no_sql_entity!();
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SystemPromptMyNoSqlEntity {
     pub value: String,
+    pub last_edited: String,
+    pub who: String,
 }
 
 impl SystemPromptMyNoSqlEntity {
