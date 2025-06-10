7d2d6c3134e44ad9066b27454fea0351e38babde
amigin <amigin@gmail.com>
2025-06-10 14:00:36 +0300

    Fixes

--- src/integration_settings/spark_post_settings.rs
+++ src/integration_settings/spark_post_settings.rs
@@ -5,5 +5,5 @@ service_sdk::macros::use_my_no_sql_entity!();
 #[enum_model(partition_key: "p", row_key:"spark-post")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SparkPostSettingsModel {
-    api_key: String,
+    pub api_key: String,
 }
