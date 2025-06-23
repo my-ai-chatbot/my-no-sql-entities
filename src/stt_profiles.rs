056519cb4d33531dfe59e6169aac1ce558984352
amigin <amigin@gmail.com>
2025-06-23 14:42:07 +0300

    stt-profiles

--- src/stt_profiles.rs
+++ src/stt_profiles.rs
@@ -2,13 +2,13 @@ use serde::*;
 
 service_sdk::macros::use_my_no_sql_entity!();
 
-#[my_no_sql_entity("stt-config")]
+#[my_no_sql_entity("stt-profiles")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct SttConfigMyNoSqlEntity {
+pub struct SttProfileMyNoSqlEntity {
     pub dest_id: String,
 }
 
-impl SttConfigMyNoSqlEntity {
+impl SttProfileMyNoSqlEntity {
     pub fn get_profile_id(&self) -> &str {
         &self.partition_key
     }
