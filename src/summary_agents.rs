77cc475d676c9bd34e4f2f050530c370ad8b6c8a
amigin <amigin@gmail.com>
2025-08-30 16:00:34 +0300

    Fixes

--- src/summary_agents.rs
+++ src/summary_agents.rs
@@ -1,10 +1,12 @@
+// Description of AI
+// PARTITION_KEY - made as string concatenation as '{inventory_id}|{llm_model_id}'
+// ROW_KEY - id of record
+
 use std::collections::HashMap;
 
 use serde::*;
 
 service_sdk::macros::use_my_no_sql_entity!();
-
-//PARTITION_KEY - inventory
 
 #[my_no_sql_entity("summary-agents")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -24,7 +26,26 @@ pub struct SummaryAgentMyNoSqlEntity {
 }
 
 impl SummaryAgentMyNoSqlEntity {
-    pub fn get_model(&self) -> &str {
+    pub fn generate_partition_key(inventory_id: &str, llm_model_id: &str) -> String {
+        format!("{inventory_id}_{llm_model_id}")
+    }
+
+    pub fn generate_row_key(id: String) -> String {
+        id
+    }
+
+    pub fn get_inventory_id(&self) -> &str {
+        let mut parts = self.partition_key.split('|');
+        parts.next().unwrap()
+    }
+
+    pub fn get_llm_model_id(&self) -> &str {
+        let mut parts = self.partition_key.split('|');
+        parts.next();
+        parts.next().unwrap()
+    }
+
+    pub fn get_id(&self) -> &str {
         &self.row_key
     }
 }
