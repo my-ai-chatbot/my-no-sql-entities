426e5f0dcb6f60ba09054263c0f0acf85301306f
amigin <amigin@gmail.com>
2025-09-22 00:16:12 +0300

    Fixed LLM parameters

--- src/agent_settings.rs
+++ src/agent_settings.rs
@@ -18,7 +18,6 @@ pub struct AgentSettingsMyNoSqlEntity {
 
     pub temperature: Option<f64>,
     pub top_p: Option<f64>,
-    pub top_k: Option<i64>,
     pub n: Option<i64>,
     pub presence_penalty: Option<f64>,
     pub frequency_penalty: Option<f64>,
