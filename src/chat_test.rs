af508990eeaa3c805c2d95456d44a7050885ffbf
amigin <amigin@gmail.com>
2025-10-08 23:45:30 +0300

    Removed tenant - since we are going to test it globally

--- src/chat_test.rs
+++ src/chat_test.rs
@@ -8,7 +8,6 @@ pub struct ChatTestMyNoSqlEntity {
     pub description: String,
     pub chat_history: Vec<ChatHistoryEvent>,
     pub result_to_assert: String,
-    pub tenant_id: String,
     pub disabled: bool,
 }
 
