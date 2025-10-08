2664ae39412147a4a9a069be934531d6d50f41b6
amigin <amigin@gmail.com>
2025-10-08 15:46:26 +0300

    feat: Add description to ChatTestMyNoSqlEntity

--- src/chat_test.rs
+++ src/chat_test.rs
@@ -4,6 +4,7 @@ service_sdk::macros::use_my_no_sql_entity!();
 #[my_no_sql_entity("chat-tests")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ChatTestMyNoSqlEntity {
+    pub description: String,
     pub chat_history: Vec<ChatHistoryEvent>,
     pub result_to_assert: String,
     pub tenant_id: String,
