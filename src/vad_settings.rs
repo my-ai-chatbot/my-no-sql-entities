7626eed614d216e994ba237adda03f6f0c1c2984
amigin <amigin@gmail.com>
2025-11-24 12:27:33 +0200

    Added VAD Settings

--- src/vad_settings.rs
+++ src/vad_settings.rs
@@ -5,14 +5,14 @@ service_sdk::macros::use_my_no_sql_entity!();
 
 #[my_no_sql_entity("vad-settings")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct WidgetClientMyNoSqlEntity {
+pub struct VadSettingsMyNoSqlEntity {
     pub frame_size: usize,
     pub silero_threshold: f32,
     pub min_speech_duration_ms: usize,
     pub speech_pad_ms: usize,
 }
 
-impl WidgetClientMyNoSqlEntity {
+impl VadSettingsMyNoSqlEntity {
     pub const PARTITION_KEY: &'static str = "vs";
     pub const ROW_KEY: &'static str = "global";
 }
